d5285a1c7154ffe1a8c1fe39c28e68d14c073c11
Mark Davidson <markd@markspace.org>
2022-03-07 01:14:03 +0000

    add more item sheets

--- module/config.js
+++ module/config.js
@@ -1,35 +1,58 @@
 export const invisiblesun = {};
 
 invisiblesun.colors = {
-    "none":"",
-    "silver":"is.colors.silver",
-    "green":"is.colors.green",
-    "blue":"is.colors.blue",
-    "indigo":"is.colors.indigo",
-    "grey":"is.colors.grey",
-    "pale":"is.colors.pale",
-    "red":"is.colors.red",
-    "gold":"is.colors.gold",
-    "invisible":"is.colors.invisible"
+    "none": "",
+    "silver": "is.colors.silver",
+    "green": "is.colors.green",
+    "blue": "is.colors.blue",
+    "indigo": "is.colors.indigo",
+    "grey": "is.colors.grey",
+    "pale": "is.colors.pale",
+    "red": "is.colors.red",
+    "gold": "is.colors.gold",
+    "invisible": "is.colors.invisible"
 }
 
 invisiblesun.facets = {
-    "none":"",
-    "curse":"is.facets.curse",
-    "night":"is.facets.night",
-    "day":"is.facets.day",
-    "midnight":"is.facets.midnight",
-    "legacy":"is.facets.legacy",
-    "dark":"is.facets.dark",
-    "metamagic":"is.facets.metamagic",
-    "divination":"is.facets.divination",
-    "conjuration":"is.facets.conjuration",
-    "teleportation":"is.facets.teleportation",
-    "death":"is.facets.death",
-    "fire":"is.facets.fire",
-    "cold":"is.facets.cold",
-    "water":"is.facets.water",
-    "earth":"is.facets.earth",
-    "air":"is.facets.air",
-    "sound":"is.facets.sound"
+    "none": "",
+    "curse": "is.facets.curse",
+    "night": "is.facets.night",
+    "day": "is.facets.day",
+    "midnight": "is.facets.midnight",
+    "legacy": "is.facets.legacy",
+    "dark": "is.facets.dark",
+    "metamagic": "is.facets.metamagic",
+    "divination": "is.facets.divination",
+    "conjuration": "is.facets.conjuration",
+    "teleportation": "is.facets.teleportation",
+    "death": "is.facets.death",
+    "fire": "is.facets.fire",
+    "cold": "is.facets.cold",
+    "water": "is.facets.water",
+    "earth": "is.facets.earth",
+    "air": "is.facets.air",
+    "sound": "is.facets.sound"
+}
+
+invisiblesun.classes = {
+    "alpha": "is.classes.alpha",
+    "beta": "is.classes.beta",
+    "gamma": "is.classes.gamma",
+    "omega": "is.classes.omega"
+}
+
+
+invisiblesun.ranges = {
+    "touch": "is.ranges.touch",
+    "close": "is.ranges.close",
+    "near": "is.ranges.near",
+    "far": "is.ranges.far",
+    "very_far": "is.ranges.very_far"
+}
+
+invisiblesun.durations = {
+    "one_round": "is.durations.one_round",
+    "short": "is.durations.short",
+    "medium": "is.durations.medium",
+    "long": "is.durations.long"
 }
