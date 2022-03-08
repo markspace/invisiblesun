5c06cf27200df208dd497385946addb7a020920f
Mark Davidson <markd@markspace.org>
2022-03-08 01:52:04 +0000

    Use editor for some fields, use less for css

--- module/sheets/is_item_sheet.js
+++ module/sheets/is_item_sheet.js
@@ -1,4 +1,13 @@
 export default class is_item_sheet extends ItemSheet {
+
+    static get defaultOptions() {
+        return mergeObject(super.defaultOptions, {
+            width: 530,
+            height: 530,
+            classes: ["invisiblesun", "sheet", "item"]
+        });
+    }
+
     get template() {
         return `systems/invisiblesun/templates/sheets/${this.item.data.type}-sheet.html`;
     }
